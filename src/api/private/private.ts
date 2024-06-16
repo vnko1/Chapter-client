a5b535a88f7cdd4a9bec91cbcbd9b188ca1a8240
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-16 18:18:22 +0300

    save updates

--- src/api/private/private.ts
+++ src/api/private/private.ts
@@ -1,13 +1,19 @@
+import { cookies } from "next/headers";
 import { CustomError } from "@/services";
+import { getSession } from "@/lib/session";
+import { EndpointsEnum } from "@/types";
 
-export async function publicApi(endpoint: string, reqInit?: RequestInit) {
+export async function privateApi(endpoint: string, reqInit?: RequestInit) {
+  const { access_token } = await getSession();
   const reqOpt = {
     ...reqInit,
     headers: {
+      Authorization: `Bearer ${access_token}`,
       Accept: "application/json",
       "Content-Type": "application/json;charset=utf-8",
     },
   };
+
   const res = await fetch(
     process.env.NEXT_PUBLIC_SERVER_URL + endpoint,
     reqOpt
--- src/types/enum/endpoints/endpoints.enum.ts
+++ src/types/enum/endpoints/endpoints.enum.ts
@@ -9,4 +9,5 @@ export enum EndpointsEnum {
   Update_pass = "/auth/pass-upd",
   Restore_acc = "/auth/restore",
   Confirm_restore_acc = "/auth/restore/confirm",
+  Refresh_Token = "auth/refresh",
 }
