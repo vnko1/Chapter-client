83d491c5a2e00f189a2a3adc33019b17b5bf2741
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-21 13:10:26 +0300

    styles update

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -9,13 +9,14 @@ import { LogoProps } from "./Logo.type";
 const Logo: FC<LogoProps> = ({ classNames, alt }) => {
   return (
     <div className={`max-w-[120px] ${classNames}`}>
-      <Link href={LinksEnum.HOME} aria-label="Home page nav link" className="">
+      <Link href={LinksEnum.HOME} aria-label="Home page nav link">
         <Image
           width={276}
           height={74}
           src={"/svg/logo.svg"}
           alt={alt || "logo"}
           priority
+          style={{ objectFit: "cover" }}
         />
       </Link>
     </div>
