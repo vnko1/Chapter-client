485968388d2b3318452c75b68ab75d9e446682c5
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-23 17:35:45 +0300

    upd postActions

--- src/lib/actions/post/post.action.ts
+++ src/lib/actions/post/post.action.ts
@@ -2,26 +2,26 @@ import { privateApi } from "@/api";
 import { EndpointsEnum } from "@/types";
 import { tryCatchWrapper } from "@/utils";
 
-export const addPost = tryCatchWrapper(async (data: FormData) => {
-  return await privateApi(EndpointsEnum.Post, {
-    headers: {
-      Accept: "application/json",
-      "X-Requested-With": "XMLHttpRequest",
-    },
-    body: data,
-    method: "POST",
-  });
-});
+export const addPost = tryCatchWrapper(
+  async (data: FormData) =>
+    await privateApi(EndpointsEnum.Post, {
+      headers: {
+        Accept: "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      body: data,
+      method: "POST",
+    })
+);
 
 export const changePost = tryCatchWrapper(
-  async ({ postId, data }: { postId: string; data: FormData }) => {
-    return await privateApi(EndpointsEnum.Post + "/" + postId, {
+  async ({ postId, data }: { postId: string; data: FormData }) =>
+    await privateApi(EndpointsEnum.Post + "/" + postId, {
       headers: {
         Accept: "application/json",
         "X-Requested-With": "XMLHttpRequest",
       },
       body: data,
       method: "PATCH",
-    });
-  }
+    })
 );
