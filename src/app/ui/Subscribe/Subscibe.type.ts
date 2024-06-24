fb90854fed23ec61724ec47a2b1d374e178e316d
Andrii Valenko <andreyvalenko@gmail.com>
2024-06-24 16:45:16 +0300

    save updates

---
+++ src/app/ui/Subscribe/Subscibe.type.ts
@@ -0,0 +1,3 @@
+export type SubscribeProps = {
+  classNames?: string;
+};
--- src/app/ui/Subscribe/Subscribe.tsx
+++ src/app/ui/Subscribe/Subscribe.tsx
@@ -1,8 +1,19 @@
 "use client";
+import { SubscribeButton } from "@/components";
 import React, { FC } from "react";
+import { SubscribeProps } from "./Subscibe.type";
 
-const Subscribe: FC = () => {
-  return <div>Subscribe</div>;
+const Subscribe: FC<SubscribeProps> = ({ classNames }) => {
+  return (
+    <SubscribeButton
+      onHandleClick={onHandleClick}
+      variant={btnVariant}
+      className={classNames}
+      aria-label="Subscribe profile button"
+    >
+      {isFollow ? "Unfollow" : "Follow"}
+    </SubscribeButton>
+  );
 };
 
 export default Subscribe;
